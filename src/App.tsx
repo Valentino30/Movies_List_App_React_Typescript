a26fab491fdbdc4f50763fb360f650324231be78
Valentino <valentino@secret-source.eu>
2022-05-07 12:24:08 +0200

    Add api call to get movies to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const getMovies = async () => {
+      const { REACT_APP_BASE_URL, REACT_APP_API_KEY } = process.env;
+      const response = await axios.get(
+        `${REACT_APP_BASE_URL}/search/movie?api_key=${REACT_APP_API_KEY}&query=Jack+Reacher`
+      );
+      console.log({ data: response.data });
+      return response.data;
+    };
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Movie List</h1>
